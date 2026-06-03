3fb439f39abb552e40a4600cb052f564b0f906e3
F5OTT <ottmanbecuwe@gmail.com>
2026-06-03 19:33:16 +0200

    ok

---
+++ src/sql/scripts/dossiers/cleanup/drop_raw_tables.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS dossier_parlementaire_raw CASCADE;
+DROP TABLE IF EXISTS dossier_initiateur_raw CASCADE;
+DROP TABLE IF EXISTS acte_legislatif_raw CASCADE;
+DROP TABLE IF EXISTS acte_rapporteur_raw CASCADE;
+DROP TABLE IF EXISTS acte_texte_associe_raw CASCADE;
+DROP TABLE IF EXISTS acte_reunion_raw CASCADE;
+DROP TABLE IF EXISTS acte_vote_raw CASCADE;
+DROP TABLE IF EXISTS acte_decision_raw CASCADE;
